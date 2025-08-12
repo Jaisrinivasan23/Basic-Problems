8dd975ea56c6406a7b1531f886b51417f12fabe7
jaisrinivasan23 <jaiisrinivasan2305@gmail.com>
2025-08-12 21:26:27 +0530

    Implement Binary Search algorithm with input/output handling

--- Sorting/BinarySearch.java
+++ Sorting/BinarySearch.java
@@ -1,22 +1,45 @@
-class SelectionSort{
+import java.util.Arrays;
+
+public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        int tar = 5;
-        int l = 0;
-        int r = arr.length;
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 5;
         
-        while(l<r){
-            int mid = (l+r)/2;
-            if(arr[mid] == tar){
-                System.out.println("Found at "+mid);
-                break;
-            }else if(tar>arr[mid]){
-                l = mid+1;
-            }else if(tar<arr[mid]){
-                r = mid-1;
-            }else{
-                System.out.println("Not Found");
-                break;
+        System.out.println("Array: " + Arrays.toString(arr));
+        System.out.println("Target: " + target);
+        
+        int result = binarySearch(arr, target);
+        
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+    
+    static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // If target is found at mid
+            if (arr[mid] == target) {
+                return mid;
+            }
+            
+            // If target is greater, ignore left half
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore right half
+            else {
+                right = mid - 1;
             }
         }
+        
+        // Element not found
+        return -1;
+    }
 }
